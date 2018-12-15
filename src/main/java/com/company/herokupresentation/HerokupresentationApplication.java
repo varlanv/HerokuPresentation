d7bf18d1f1d47dd53eb54296fb6209abb77da28d
varlanv <varlanv96@gmail.com>
2018-12-15 04:03:57 +0200

    first commit

---
+++ src/main/java/com/company/herokupresentation/HerokupresentationApplication.java
@@ -0,0 +1,14 @@
+package com.company.herokupresentation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokupresentationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HerokupresentationApplication.class, args);
+    }
+
+}
+
